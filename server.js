03e0d116ad811dc5624d894910412f2bebbc604b
yoterz <40376255+yoterz@users.noreply.github.com>
2018-08-17 22:10:46 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,31 +3,33 @@ const Discord = require("discord.js")
 const bot = new Discord.Client()
              var stream = 'http://27.254.142.208:8100/stream'
              var clash = 'https://www.youtube.com/watch?v=3CzIsRw74Kc'
-             var shofu = 'https://www.youtube.com/watch?v=JGwWNGJdvx8'
-             // var stream = '/home/discord/audio.mp3'
-             
+          
 bot.on("ready",() => {
     console.log('Ready...')
 
 })
 
-
 bot.on ("message", (msg) => {
     if (msg.content === "radio"){
-       playradio(stream,"เปิดวิทยุ..สถานี RequestRadio.in.th")
+        playradio(stream,"เปิดวิทยุ..สถานี RequestRadio.in.th")
     }  
 
     if (msg.content === "ใจเย็นเย็น"){
-        playsong(clash,"เพลง ใจเย็นเย็น  CLASH")
+        console.log(clash)
+        playsong(clash)
     }  
-
- 
+  
+    if (msg.content.startsWith(".p")){
+        let args = msg.content.split(' ').slice(1)
+        console.log(args[0])
+        playsong(args[0])
+    }
+    
     if (msg.content ==="end"){
           var voiceChannel = msg.member.voiceChannel
         if (voiceChannel) {
           voiceChannel.leave()
           sendEmbed("ปิดวิทยุ..")
-          
         }
     }
 
@@ -42,36 +44,35 @@ bot.on ("message", (msg) => {
 
  function playradio(stream,msgz){        
     var voiceChannel = msg.member.voiceChannel
-  if (voiceChannel) {
-         voiceChannel.join()
-      
-        .then(connection => {
-             let dispatcher = connection.playStream(stream)
-             sendEmbed(msgz)
-             
-          })
-        .catch(console.log);
-    } else {
-      msg.reply('คุณไม่ได้อยู่ในห้อง Voice Channel');
-  }
+    if (voiceChannel) {
+           voiceChannel.join()
+           .then(connection => {
+               let dispatcher = connection.playStream(stream)
+               sendEmbed(msgz)
+            })
+          .catch(console.log);
+      } else {
+        msg.reply('คุณไม่ได้อยู่ในห้อง Voice Channel');
+    }
 }
      
-function playsong(url,msgz){        
+function playsong(url){        
   var voiceChannel = msg.member.voiceChannel
-if (voiceChannel) {
-       voiceChannel.join()
-    
-      .then(connection => {
-           let stream = ytdl(url, { filter: 'audioonly' });
-             
-           let dispatcher = connection.playStream(stream)
-           sendEmbed(msgz)
-          
-        })
-      .catch(console.log);
-  } else {
-    msg.reply('คุณไม่ได้อยู่ในห้อง Voice Channel');
-}
+    if (voiceChannel) {
+           voiceChannel.join()
+
+          .then(connection => {
+               let stream = ytdl(url, { filter: 'audioonly' });
+               let dispatcher = connection.playStream(stream)
+               ytdl.getInfo(url, function(err, info) {
+                    console.log(info.title) // "Adele - Hello"
+                    sendEmbed(info.title)
+                  });
+            })
+          .catch(console.log);
+      } else {
+        msg.reply('คุณไม่ได้อยู่ในห้อง Voice Channel');
+    }
 }
     
 })
