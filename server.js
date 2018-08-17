ea94c5f08e780296be1fec2cbdc1aa2a25f222db
yoterz <40376255+yoterz@users.noreply.github.com>
2018-08-17 18:33:42 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,10 @@
 const ytdl = require('ytdl-core');
 const Discord = require("discord.js")
 const bot = new Discord.Client()
-
+             var stream = 'http://27.254.142.208:8100/stream'
+             // var stream = ytdl('https://www.youtube.com/watch?v=JGwWNGJdvx8',{ filter: 'audioonly' })
+             // var stream = '/home/discord/audio.mp3'
+             
 bot.on("ready",() => {
     console.log('Ready...')
 
@@ -14,33 +17,32 @@ bot.on("ready",() => {
 bot.on ("message", (msg) => {
     if (msg.content === "radio"){
       // msg.reply('เปิดวิทยุ..');
-       msg.channel.send('@here เปิดวิทยุ..สถานี RequestRadio.in.th');
-        playradio()
-        
-function playradio(){  
+       
       var voiceChannel = msg.member.voiceChannel
       if (voiceChannel) {
-            voiceChannel.join()
+           voiceChannel.join()
         
           .then(connection => {
-          
-                
-             var stream = 'http://27.254.142.208:8100/stream'
-             // var stream = ytdl('https://www.youtube.com/watch?v=JGwWNGJdvx8',{ filter: 'audioonly' })
-             // var stream = '/home/discord/audio.mp3'
-            const dispatcher = connection.playStream(stream)
-                                   
-          dispatcher.on("end", end => {
-                  playradio()
-                  console.log('เริ่มเล่นใหม่...')
-            });
-                
-          })
+               msg.channel.send('@here เปิดวิทยุ..สถานี RequestRadio.in.th');
+                playradio()
+            })
           .catch(console.log);
       } else {
         msg.reply('คุณไม่ได้อยู่ในห้อง Voice Channel');
       }
      }
+               
+               
+function playradio(){  
+
+          const dispatcher = connection.playStream(stream)
+                                   
+                 dispatcher.on("end", end => {
+                        playradio()
+                        console.log('เริ่มเล่นใหม่...')
+                 });
+                
+          
 }
     if (msg.content ==="end"){
           var voiceChannel = msg.member.voiceChannel
